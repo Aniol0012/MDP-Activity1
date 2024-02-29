336c3edd65fbab1f4bae9238a5bb1e560dcf1fd0
Aniol0012 <aniol0012@gmail.com>
2024-02-29 17:14:24 +0100

    refactor: renamed directories

--- mdpActivity/asgi.py
+++ mdpActivity/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoProject1 project.
+ASGI config for mdpActivity project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mdpActivity.settings')
 
 application = get_asgi_application()
