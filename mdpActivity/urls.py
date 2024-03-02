1f55d9dfd32bbc16066b3a5d9f4852fa618b8975
Aniol <aniol0012@gmail.com>
2024-03-02 03:22:29 +0100

    refactor: renamed admin panel endpoint

--- mdpActivity/urls.py
+++ mdpActivity/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('contalt/', admin.site.urls),
+    path('blogadmin/', admin.site.urls),
     path('', include('posts.urls')),
 ]
