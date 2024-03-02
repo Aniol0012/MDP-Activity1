1e2c7bedeff6d99c8ebcda571babb887e482b4fc
Aniol0012 <aniol0012@gmail.com>
2024-03-02 15:29:27 +0100

    style: prettified code

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,30 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [2, 'always', [
-      'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'translation', 'security', 'changeset', 'init'
-    ]],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
-    'header-max-length': [0, 'always', 72]
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "translation",
+        "security",
+        "changeset",
+        "init",
+      ],
+    ],
+    "subject-full-stop": [0, "never"],
+    "subject-case": [0, "never"],
+    "header-max-length": [0, "always", 72],
   },
-  ignores: [(commit) => commit.startsWith('Merge')],
+  ignores: [(commit) => commit.startsWith("Merge")],
 };
