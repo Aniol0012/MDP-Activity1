65d62f077bc059deef0e129e274dd915d16855b0
Aniol0012 <aniol0012@gmail.com>
2024-03-03 01:04:51 +0100

    fix: fixed python linter

--- posts/tests.py
+++ posts/tests.py
@@ -5,7 +5,7 @@ from posts.models import Post
 # Create your tests here.
 
 def test_post_creation(self):
-    post = Post.objects.create(title='Test Post', content='This is a test post.')
+    post = Post.objects.create(title='Test Post')
     self.assertEqual(post.title, 'Test Post')
     self.assertEqual(post.content, 'This is a test post.')
     self.assertEqual(post.__str__(), post.title)
@@ -20,7 +20,7 @@ def test_post_list_view(self):
 
 
 def test_post_detail_view(self):
-    post = Post.objects.create(title='Test Post', content='This is a test post.')
+    post = Post.objects.create(title='Test Post')
     response = self.client.get(f'/post/{post.pk}/')
     self.assertEqual(response.status_code, 200)
     self.assertTemplateUsed(response, 'posts/post_detail.html')
@@ -34,7 +34,7 @@ def test_post_create_view(self):
     self.client.login(username='tester', password='12345')
     response = self.client.get('/post/create/')
     self.assertEqual(response.status_code, 200)
-    response = self.client.post('/post/create/', {'title': 'Test Post', 'content': 'This is a test post.'})
+    response = self.client.post('/post/create/', {'title': 'Test Post'})
     self.assertEqual(response.status_code, 302)
     self.assertEqual(Post.objects.count(), 1)
     post = Post.objects.first()
