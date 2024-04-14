e1ef91287d6c713731790960da9efaa222daab18
Aniol0012 <aniol0012@gmail.com>
2024-04-14 22:34:59 +0200

    feat: add vercel deployment

--- mdpActivity/settings.py
+++ mdpActivity/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ('django-insecure-%dhpuf4pnfyeob#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 # Application definition
 
